909b3c22b27ce673f40bfa0c2c6c8f5c82c3f783
Zin Lin Phyo <zinlin.phyo@flymya.co>
2022-08-02 15:52:57 +0630

    [inprogress] bead counter refactor

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,48 +1,91 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, SafeAreaView, StyleSheet, Text, View, StatusBar, TouchableOpacity, ScrollView } from 'react-native';
-import moment from 'moment';
-import { Calendar } from 'react-native-calendars';
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import * as Haptics from 'expo-haptics';
+import React, { useState, useEffect } from "react";
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import * as Haptics from "expo-haptics";
+import { goneTaw } from "../data/GoneTaw";
 
 export default function HomeScreen() {
-
   const [counter, setCounter] = useState(0);
+  const cycle = 9;
+  const beadsInCycle = 108;
 
-  useEffect(() => { counter % 108 === 0 ? Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error) : counter % 10 === 0 ? Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success) : Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light) }, [counter]);
+  useEffect(() => {
+    counter % 108 === 0
+      ? Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
+      : counter % 10 === 0
+      ? Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
+      : Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+  }, [counter]);
 
-  const beadClickHandler = () => { setCounter(counter + 1); };
+  const beadClickHandler = () => {
+    setCounter(counter + 1);
+  };
 
-  const description = "၂။ သမ္မာသမ္ဗုဒ္ဓေါ ဂုဏ်တော် \n တရားအလုံးစုံကို သဗ္ဗညုတဉာဏ်ဖြင့် အလိုလိုသိခြင်း၊ ဉာဏ်ပညာအကြီးဆုံးပုဂ္ဂိုလ်ဖြစ်တော်မူသော မြတ်စွာဘုရား။ \n (က) သမ္မာသမ္ဗုဒ္ဓေါ – သိစရာအားလုံးကို အမှန်အတိုင်း ကိုယ်ပိုင်ဉာဏ်ဖြင့် သိတော်မူသော မြတ်စွာဘုရား။ မဖေါက်မပြန် သယမ္ဘူဉာဏ်ဖြင့် ကိုယ်တော်တိုင်သာ အလုံးစုံသောတရားတို့ကို သိတော်မူပြီးသော မြတ်စွာဘုရား။ \n (ခ) သမ္မာသမ္ဗုဒ္ဓေါ – ကိုယ်တော်တိုင်သာလျှင် ဝါသနာနှင့်တကွ မောဟတည်းဟူသော အိပ်ပျော်ခြင်းမှ နိုးတော်မူပြီးသော မြတ်စွာဘုရား။ \n (ဂ) သမ္မာသမ္ဗုဒ္ဓေါ – အရဟတ္တမဂ်ဉာဏ်နှင့်ပေါင်းဖော်ရခြင်းကြောင့် သဗ္ဗညုတဉာဏ်သို့ရောက်သဖြင့် အသင့်အားဖြင့် ကိုယ်တော်တိုင်သာ ပွင့်တော်မူပြီးသော မြတ်စွာဘုရား။"
+  const resetHandler = () => {
+    setCounter(0);
+  };
+
+  const description = goneTaw[0].description;
 
   return (
     <View style={styles.container}>
       <ExpoStatusBar style="auto" />
-      <SafeAreaView style={{ flex: 1, paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 24, margin: 20 }}>သမ္မာသမ္ဗုဒ္ဓေါ သိဒ္ဓိ (၉ ပတ်)</Text>
+      <SafeAreaView style={background}>
+        <Text style={title}>{goneTaw[0].title} သိဒ္ဓိ (၉ ပတ်)</Text>
         <Text style={{ fontSize: 36, margin: 20 }}>{counter}</Text>
-        <Text style={{ fontSize: 18 }}>108 x {parseInt(counter / 108)}</Text>
-        <TouchableOpacity style={{ backgroundColor: 'gray', width: 200, height: 200, justifyContent: 'center', alignItems: 'center', borderRadius: 100, margin: 50 }} onPress={beadClickHandler}>
-          <Text style={{ color: 'white', fontSize: 24, fontWeight: 'bold' }}>
-            {counter}
-          </Text>
+        <Text style={{ fontSize: 18 }}>
+          {beadsInCycle} x {parseInt(counter / beadsInCycle)}
+        </Text>
+        <TouchableOpacity style={beadCounter} onPress={beadClickHandler}>
+          <Text style={beadText}>{counter}</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{}} onPress={() => setCounter(0)}>
-          <Text style={{ color: 'black', fontSize: 24, fontWeight: 'bold' }}>
-            Reset
-          </Text>
+        <TouchableOpacity style={{}} onPress={resetHandler}>
+          <Text style={reset}>Reset</Text>
         </TouchableOpacity>
-        <ScrollView style={{ flex: 1, width: '100%', height: 'auto', paddingHorizontal: 10 }}>
+        <ScrollView style={description}>
           <Text>{description}</Text>
         </ScrollView>
       </SafeAreaView>
-    </View >
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
+  background: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: { fontSize: 24, margin: 20 },
+  beadCounter: {
+    backgroundColor: "gray",
+    width: 200,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    margin: 50,
+  },
+  beadText: { color: "white", fontSize: 24, fontWeight: "bold" },
+  description: {
+    flex: 1,
+    width: "100%",
+    height: "auto",
+    paddingHorizontal: 10,
+  },
+  reset: { color: "black", fontSize: 24, fontWeight: "bold" },
 });
